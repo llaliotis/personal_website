b8e59dc2d939ea59cc746edfd0581e5101ebb79b
llaliotis <llaliotis@gmail.com>
2023-02-06 13:14:47 +0200

    Updated code for website preview

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,19 +12,19 @@ class MyDocument extends Document {
         <Head>
             <link rel="preconnect" href="https://fonts.googleapis.com"/>
             <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"/>
-            <meta property="og:title" content="Nickolas Tazes"/>
-            <meta property="og:description" content="Founder of Readnet Publications"/>
+            <meta property="og:title" content="Eleftherios Laliotis"/>
+            <meta property="og:description" content="Founder of Swollet"/>
             <meta property="og:type" content="website"/>
-            <meta property="og:url" content="https://tazes.me"/>
+            <meta property="og:url" content="https://lalioits.me"/>
             <meta property="og:image" content="/ogpersonal.png"/>
             <link rel="icon" href="/Group 219.png"/>
-            <meta name="twitter :site" content="@nickolas_tazes" />
-            <meta name= "twitter:creator" content="@nickolas_tazes"/>
-            <meta property="og:url" content=" https://tazes.me"/>
+            <meta name="twitter :site" content="@greekdubliner" />
+            <meta name= "twitter:creator" content="@greekdubliner"/>
+            <meta property="og:url" content=" https://laliotis.me"/>
             <meta name="twitter:card" content="summary_large_image"/>
-            <meta name="twitter:image:alt" content="orange tazes in a black background"/>
-            <meta name="twitter:title" content="Nickolas Tazes"/>
-            <meta name="twitter:description" content="Founder of Readnet Publications. Since the start of 2020, I have envisioned, organized, developed, and launched a series of innovative projects. My goal is to solve some of the biggest social & economical problems in the world. The list of projects includes"/>
+            <meta name="twitter:image:alt" content="emerald laliotis in a black background"/>
+            <meta name="twitter:title" content="Eleftherios Laliotis"/>
+            <meta name="twitter:description" content="Founder of Swollet. Since the beginning of 2020, I have been creating and implementing a range of groundbreaking initiatives with the aim of addressing significant social and economic issues. The projects I have undertaken include:"/>
             <meta name="twitter:image" content="https://tazes.me/ogpersonal.png"/>
         </Head>
         <body>
