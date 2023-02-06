c37ba1d74dd8f6e918c9aa3b5a901ceeeb827781
llaliotis <llaliotis@gmail.com>
2023-02-06 12:32:58 +0200

    Updated post preview

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -28,12 +28,12 @@ function index({}: Props) {
               <h1 className='text-slate-100 font-semibold text-3xl text-start px-4 lg:px-8'>Posts</h1>
 
               <article className='my-7 mx-4 lg:mx-8 border border-[#292929] py-3 px-2 bg-[#202020] rounded-[12px]'>
-              <a href="/blog/regulating-the-frontends-of-Decentralized-Exchanges">
-                <Image src={'/Regulating the frontends of Decentralized Exchanges.png'} height={200} width={600} className='w-full object-cover rounded-[8px]' />
+              <a href="/blog/we-may-be-due-for-a-different-kind-of-recession">
+                <Image src={'/Recession_cover_image.png'} height={200} width={600} className='w-full object-cover rounded-[8px]' />
               <div className='items-center space-y-2'>
-                <h2 className='text-slate-100 font-semibold text-xl text-left'>Regulating the frontends of Decentralized Exchanges</h2>
-                <p className='text-slate-300 text-sm text-left'>2022-12-05</p>
-                <p className='text-slate-300 text-sm text-left'>My answer: Enforce a quiz with some DEX-related questions for each wallet. If the wallet owner answers at least 3/5 of them, then give them a Soulbound token and let them interact with the protocol without any limitations. In some cases, maybe add another quiz with 2-3 questions about more complex actions like providing liquidity...</p>
+                <h2 className='text-slate-100 font-semibold text-xl text-left'>We may be due for a different kind of recession</h2>
+                <p className='text-slate-300 text-sm text-left'>2022-09-26</p>
+                <p className='text-slate-300 text-sm text-left'>It’s no secret that the economy has been struggling for the past few years. Recession has been a dirty word, and it seems like we’re always just on the brink of another one. But, we may be due for a different kind of recession...</p>
               </div>
               </a>
               </article>
